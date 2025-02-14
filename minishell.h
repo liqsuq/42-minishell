809f1da4f6e33d05c5abfe7a36543ba6a4debfbd
Kazuki Adachi <kadachi@student.42tokyo.jp>
2025-02-14 15:55:44 +0900

    [REF] norm

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kadachi <kadachi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 18:44:12 by kadachi           #+#    #+#             */
-/*   Updated: 2025/02/14 14:32:57 by kadachi          ###   ########.fr       */
+/*   Updated: 2025/02/14 15:55:32 by kadachi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,20 @@
 
 # define PROMPT "minish$ "
 
-typedef enum	e_kind
+typedef enum e_kind
 {
 	WORD,
 	META,
 	EOL,
 }	t_kind;
 
-
-typedef struct	s_token
+typedef struct s_token
 {
 	char			*word;
 	t_kind			kind;
 	struct s_token	*next;
 }					t_token;
-	
+
 typedef struct s_data
 {
 	int	exit_status;
