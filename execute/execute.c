05b61946d4a516dd3b4025925d9baebf51f83514
Kazuki Adachi <kadachi@student.42tokyo.jp>
2025-03-12 16:36:16 +0900

    [REF] exec_command()をexeccmd()で置換

--- execute/execute.c
+++ execute/execute.c
@@ -2,46 +2,27 @@
 
 #include "minishell.h"
 
-// static void execcmd(t_node *node)
-// {
-// 	char **argv;
-// 	char *path;
-	
-// 	argv = new_argv(node->args);
-// 	if (!argv[0])
-// 	{
-// 		free_argv(argv);
-// 		return (0);
-// 	}
-// 	path = resolve_path(argv[0]);
-// 	if (!path)
-// 	{
-// 		free_argv(argv);
-// 		return (127);
-// 	}
-// 	if (execve(path, argv, NULL))
-// 	{
-// 		if (errno == ENOENT)
-// 			exit(127);
-// 		else if (errno == EACCES || errno == ENOEXEC)
-// 			exit(126);
-// 		else
-// 			exit(EXIT_FAILURE);
-// 	}
-// }
-
-static int	exec_command(char *path, char **argv)
+static void execcmd(t_node *node)
 {
-	extern char	**environ;
-	pid_t		pid;
-	int			status;
-
-	pid = fork();
-	if (pid < 0)
-		fatal_error("fork");
-	if (pid == 0)
+	char **argv;
+	char *path;
+	
+	argv = new_argv(node->args);
+	if (!argv[0])
 	{
-		execve(path, argv, environ);
+		free_argv(argv);
+		exit (1);
+	}
+	path = resolve_path(argv[0]);
+	if (!path)
+	{
+		free_argv(argv);
+		exit (127);
+	}
+	if (execve(path, argv, NULL))
+	{
+		free_argv(argv);
+		free(path);
 		if (errno == ENOENT)
 			exit(127);
 		else if (errno == EACCES || errno == ENOEXEC)
@@ -49,39 +30,25 @@ static int	exec_command(char *path, char **argv)
 		else
 			exit(EXIT_FAILURE);
 	}
-	waitpid(pid, &status, 0);
-	return (WEXITSTATUS(status));
 }
 
 int	execute(t_node *node)
 {
-	int	status;
-	char **argv;
+	pid_t	pid;
+	int		status;
 
 	if (!node)
 		return (0);
-	argv = new_argv(node->args);
-	if (!argv[0])
-	{
-		free_argv(argv);
-		return (0);
-	}
 	status = 0;
 	if (has_pipe(node))
 		return (exec_pipeline(node));
-	else
-	{
-		redirect(node->redirects, NULL);
-		char	*path = resolve_path(argv[0]);
-		if (!path)
-			status = 127;
-		else
-		{
-			status = exec_command(path, argv);
-			free(path);
-		}
-		free_argv(argv);
-		reset_redirect(node->redirects);
-		return (status);
-	}
+	redirect(node->redirects, NULL);
+	pid = fork();
+	if (pid < 0)
+		fatal_error("fork");
+	if (pid == 0)
+		execcmd(node);
+	waitpid(pid, &status, 0);
+	reset_redirect(node->redirects);
+	return (WEXITSTATUS(status));
 }
