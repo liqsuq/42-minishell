e9a89996e3bb8f407166f70a98a01978e8cfa91b
liqsuq <liqsuq@rkgk.net>
2025-04-15 14:13:54 +0900

    [REF] 書き方を他と合わせる

--- expand/expand_quote.c
+++ expand/expand_quote.c
@@ -8,9 +8,9 @@ static void	remove_quote(t_token *token)
 	char	*cur;
 	char	c;
 
-	new_word = ft_strdup("");
+	new_word = ft_calloc(1, sizeof(char));
 	if (new_word == NULL)
-		fatal_error("ft_strdup");
+		fatal_error("ft_calloc");
 	cur = token->word;
 	while (*cur != '\0' && !is_metacharacter(*cur))
 	{
