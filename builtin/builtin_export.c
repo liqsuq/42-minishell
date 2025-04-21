9312ebe7298ac5e64bb12ac33e26a8f26d4c3fa5
Killua <106412328+Killua0615@users.noreply.github.com>
2025-04-21 18:04:41 +0900

    [RFC] コードをより短く

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -2,24 +2,6 @@
 
 #include "minishell.h"
 
-static int is_valid_identifier(const char *name)
-{
-	int i;
-
-	if (!name || !name[0])
-		return (0);
-	if (!ft_isalpha((unsigned char)name[0]) && name[0] != '_')
-		return (0);
-	i = 1;
-	while (name[i] != '\0')
-	{
-		if (!ft_isalnum((unsigned char)name[i]) && name[i] != '_')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 void print_env_export_format(t_env *env)
 {
   while (env)
@@ -30,70 +12,72 @@ void print_env_export_format(t_env *env)
   }
 }
 
-static void process_arg_val(t_data *data, const char *arg)
+static int is_valid_identifier(const char *name, size_t len)
 {
-	char   *eq;
-	size_t key_len;
-	char   *key_dup;
+	size_t i;
 
-	eq = ft_strchr(arg, '=');
-	if (eq == NULL)
-	{
-		ft_dprintf(STDERR_FILENO, HEADER "export: '=' missing\n");
-		data->exit_status = 1;
-		return;
-	}
-	key_len = (size_t)(eq - arg);
-	key_dup = ft_substr(arg, 0, key_len);
-	if (key_dup	== NULL)
-	{
-		data->exit_status = 1;
-		return;
-	}
-	set_env(&data->env, key_dup, eq + 1);
-	free(key_dup);
+	if (len == 0)
+		return (0);
+	if (!ft_isalpha((unsigned char)name[0]) && name[0] != '_')
+		return (0);
+	i = 0;
+	while (++i < len)
+		if (!ft_isalnum((unsigned char)name[i]) && name[i] != '_')
+			return (0);
+	return (1);
 }
 
-static void check_arg(t_data *data, const char *arg)
+static int check_name(t_data *data, const char *arg)
 {
 	char   *eq;
 	size_t  key_len;
-	char   *key;
 
 	eq = ft_strchr(arg, '=');
 	if (eq)
 		key_len = (size_t)(eq - arg);
 	else
 		key_len = ft_strlen(arg);
-	key = ft_substr(arg, 0, key_len);
-	if (key == NULL)
-	{
-		data->exit_status = 1;
-		return;
-	}
-	if (!is_valid_identifier(key))
+	if (!is_valid_identifier(arg, key_len))
 	{
 		ft_dprintf(STDERR_FILENO, HEADER "export: not a valid identifier\n");
 		data->exit_status = 1;
-		free(key);
+		return 0;
+	}
+	return 1;
+}
+
+static void process_arg(t_data *data, const char *arg)
+{
+	char   *eq;
+	size_t key_len;
+	char   *key;
+
+	eq = ft_strchr(arg, '=');
+	if (eq == NULL)
+		return;
+	key_len = (size_t)(eq - arg);
+	key = ft_substr(arg, 0, key_len);
+	if (key	== NULL)
+	{
+		data->exit_status = 1;
 		return;
 	}
+	set_env(&data->env, key, eq + 1);
 	free(key);
-	process_arg_val(data, arg);
 }
 
 void builtin_export(t_data *data, char **argv)
 {
 	int i;
 
+	data->exit_status = 0;
 	if (!argv[1])
 	{
 		print_env_export_format(data->env);
-		data->exit_status = 0;
 		return;
 	}
-	data->exit_status = 0;
 	i = 0;
 	while (argv[++i])
-		check_arg(data, argv[i]);
+		if(check_name(data, argv[i]))
+			process_arg(data, argv[i]);
 }
