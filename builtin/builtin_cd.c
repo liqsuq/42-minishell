7ad526581234c5bd1f4b992a4c5419e1b4eec841
Killua <106412328+Killua0615@users.noreply.github.com>
2025-04-24 02:19:50 +0900

    [WIP] cd,pwd途中

--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -6,90 +6,88 @@
  * PWD が無い場合は getcwd() を使う。
  * chdir に成功したら、PWD が unset されていなければ更新する。
  */
-static void cd_to_path(t_data *data, const char *path)
+static void cd_to_path(t_data *data, const char *path, char *argv)
 {
-    // まず、現在の PWD を取得
-    char *oldpwd = get_env(data->env, "PWD");
+	// まず、現在の PWD を取得
+	char *oldpwd = get_env(data->env, "PWD");
 
-    // chdir 実行
-    if (chdir(path) < 0)
-    {
-        ft_dprintf(STDERR_FILENO, HEADER "cd: %s: %s\n", path, strerror(errno));
-        data->exit_status = 1;
-        return;
-    }
+	// chdir 実行
+	if (chdir(path) < 0)
+	{
+		ft_dprintf(STDERR_FILENO, "cd: no such file or directory: %s\n", argv);
+		data->exit_status = 1;
+		return;
+	}
 
-    // chdir 成功 → 必要なら OLDPWD を更新
-    // Bash 互換を目指すなら、PWD が unset でもここで OLDPWD を更新する
-    if (oldpwd != NULL)
-        set_env(&data->env, "OLDPWD", oldpwd);
+	// chdir 成功 → 必要なら OLDPWD を更新
+	// Bash 互換を目指すなら、PWD が unset でもここで OLDPWD を更新する
+	if (oldpwd != NULL)
+		set_env(&data->env, "OLDPWD", oldpwd);
 
-    // さらに、PWD が unset されていなければ getcwd() で新しいディレクトリを設定
-    if (get_env(data->env, "PWD") != NULL)
-    {
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) == NULL)
-        {
-            ft_dprintf(STDERR_FILENO, HEADER "cd: getcwd: %s\n", strerror(errno));
-            data->exit_status = 1;
-            return;
-        }
-        set_env(&data->env, "PWD", cwd);
-    }
-    data->exit_status = 0;
+	// さらに、PWD が unset されていなければ getcwd() で新しいディレクトリを設定
+	if (get_env(data->env, "PWD") != NULL)
+	{
+		char cwd[PATH_MAX];
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+		{
+			ft_dprintf(STDERR_FILENO, HEADER "cd: getcwd: %s\n", strerror(errno));
+			data->exit_status = 1;
+			return;
+		}
+		set_env(&data->env, "PWD", cwd);
+	}
+	data->exit_status = 0;
 }
-
 
 void builtin_cd(t_data *data, char **argv)
 {
-    if (argv[1] == NULL)
-    {
-        // 引数がない場合: HOME を使う
-        char *home = get_env(data->env, "HOME");
-        if (!home)
-        {
-            ft_dprintf(STDERR_FILENO, HEADER "cd: HOME not set\n");
-            data->exit_status = 1;
-            return;
-        }
-        cd_to_path(data, home);
-        return;
-    }
+	if (argv[1] == NULL)
+	{
+		// 引数がない場合: HOME を使う
+		char *home = get_env(data->env, "HOME");
+		if (!home)
+		{
+			ft_dprintf(STDERR_FILENO, HEADER "cd: HOME not set\n");
+			data->exit_status = 1;
+			return;
+		}
+		// ユーザ入力 (argv[1]) はNULLだが、一応そのまま渡す
+		cd_to_path(data, home, argv[1]);
+		return;
+	}
 
-    // (2つ以上の引数は無視するなど、簡易的実装)
+	// argv[1] が絶対パスか相対パスかを判定
+	if (argv[1][0] == '/')
+	{
+		// 絶対パス
+		// 第3引数にユーザ入力の相対パスを渡す
+		cd_to_path(data, argv[1], argv[1]);
+	}
+	else
+	{
+		// 相対パスを絶対パスに結合
+		char *pwd = get_env(data->env, "PWD");
+		if (pwd == NULL)
+		{
+			char cwd[PATH_MAX];
+			if (getcwd(cwd, sizeof(cwd)) == NULL)
+			{
+				ft_dprintf(STDERR_FILENO, HEADER "cd: getcwd: %s\n", strerror(errno));
+				data->exit_status = 1;
+				return;
+			}
+			pwd = cwd;
+		}
+		char *tmp = ft_strjoin(pwd, "/");
+		if (!tmp)
+			fatal_error("malloc");
+		char *path = ft_strjoin(tmp, argv[1]);
+		free(tmp);
+		if (!path)
+			fatal_error("malloc");
 
-    // argv[1] が絶対パスか相対パスかを判定
-    if (argv[1][0] == '/')
-    {
-        // 絶対パス
-        cd_to_path(data, argv[1]);
-    }
-    else
-    {
-        // 相対パス
-        char *pwd = get_env(data->env, "PWD");
-        if (pwd == NULL)
-        {
-            // PWD が unset されている場合は getcwd()
-            char cwd[PATH_MAX];
-            if (getcwd(cwd, sizeof(cwd)) == NULL)
-            {
-                ft_dprintf(STDERR_FILENO, HEADER "cd: getcwd: %s\n", strerror(errno));
-                data->exit_status = 1;
-                return;
-            }
-            pwd = cwd;
-        }
-        // 「pwd + "/" + argv[1]」をつないだパスを生成
-        char *tmp = ft_strjoin(pwd, "/");
-        if (!tmp)
-            fatal_error("malloc");
-        char *path = ft_strjoin(tmp, argv[1]);
-        free(tmp);
-        if (!path)
-            fatal_error("malloc");
-
-        cd_to_path(data, path);
-        free(path);
-    }
+		// 第3引数にユーザ入力 (argv[1]) を渡す
+		cd_to_path(data, path, argv[1]);
+		free(path);
+	}
 }
--- builtin/builtin_pwd.c
+++ builtin/builtin_pwd.c
@@ -8,27 +8,27 @@
  */
 void builtin_pwd(t_data *data, char **argv)
 {
-    (void)argv;  // argv[1] などオプションは今回は単純化し、無視する例
+	(void)argv;  // argv[1] などオプションは今回は単純化し、無視する例
 
-    char *pwd_env = get_env(data->env, "PWD");
-    if (pwd_env)
-    {
-        // unset されていない PWD が存在
-        ft_putendl_fd(pwd_env, STDOUT_FILENO);
-        data->exit_status = 0;
-        return;
-    }
-    else
-    {
-        // PWD が無い場合は getcwd()
-        char cwd[PATH_MAX];
-        if (getcwd(cwd, sizeof(cwd)) == NULL)
-        {
-            ft_dprintf(STDERR_FILENO, HEADER "pwd: getcwd: %s\n", strerror(errno));
-            data->exit_status = 1;
-            return;
-        }
-        ft_putendl_fd(cwd, STDOUT_FILENO);
-        data->exit_status = 0;
-    }
+	char *pwd_env = get_env(data->env, "PWD");
+	if (pwd_env)
+	{
+		// unset されていない PWD が存在
+		ft_putendl_fd(pwd_env, STDOUT_FILENO);
+		data->exit_status = 0;
+		return;
+	}
+	else
+	{
+		// PWD が無い場合は getcwd()
+		char cwd[PATH_MAX];
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+		{
+			ft_dprintf(STDERR_FILENO, HEADER "pwd: getcwd: %s\n", strerror(errno));
+			data->exit_status = 1;
+			return;
+		}
+		ft_putendl_fd(cwd, STDOUT_FILENO);
+		data->exit_status = 0;
+	}
 }
