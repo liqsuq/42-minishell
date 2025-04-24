80239086e082cdfe1e28681742eac0e88fe302b3
liqsuq <liqsuq@rkgk.net>
2025-04-24 10:23:12 +0900

    [REF] open/attch/detach_pipe()の追加

--- pipeline/pipeline.c
+++ pipeline/pipeline.c
@@ -18,39 +18,51 @@ static void	move_fd(int src, int dst)
 	close_fd(src);
 }
 
+static void open_pipe(t_node *node, int *pipefd)
+{
+	if (has_pipe(node))
+		if (pipe(pipefd) < 0)
+			fatal_error("setup_pipe");
+}
+
+static void attach_pipe(t_node *node, int prev_pipeout, int *pipefd)
+{
+	if (prev_pipeout != -1)
+		move_fd(prev_pipeout, STDIN_FILENO);
+	if (has_pipe(node))
+	{
+		close_fd(pipefd[0]);
+		move_fd(pipefd[1], STDOUT_FILENO);
+	}
+}
+
+static void detach_pipe(t_node *node, int prev_pipeout, int *pipefd)
+{
+	if (prev_pipeout != -1)
+		close_fd(prev_pipeout);
+	if (has_pipe(node))
+		close_fd(pipefd[1]);
+}
+
 int	pipeline(t_data *data, t_node *node, int prev_pipeout)
 {
 	int		pipefd[2];
 	pid_t	pid;
 
-	if (has_pipe(node))
-		if (pipe(pipefd) < 0)
-			fatal_error("pipe");
+	open_pipe(node, pipefd);	
 	pid = fork();
 	if (pid < 0)
 		fatal_error("fork");
 	if (pid == 0)
 	{
+		attach_pipe(node, prev_pipeout, pipefd);
 		reset_signal();
-		if (prev_pipeout != -1)
-			move_fd(prev_pipeout, STDIN_FILENO);
-		if (has_pipe(node))
-		{
-			close_fd(pipefd[0]);
-			move_fd(pipefd[1], STDOUT_FILENO);
-		}
-		if (is_builtin(node->args))
-		{
-			execute_builtin(data, node);
-			exit(data->exit_status);
-		}
-		else
+		if (!is_builtin(node->args))
 			execute_command(data, node);
+		execute_builtin(data, node);
+		exit(data->exit_status);
 	}
-	if (prev_pipeout != -1)
-		close_fd(prev_pipeout);
-	if (has_pipe(node))
-		close_fd(pipefd[1]);
+	detach_pipe(node, prev_pipeout, pipefd);
 	if (has_pipe(node))
 		return (pipeline(data, node->next, pipefd[0]));
 	return (pid);
