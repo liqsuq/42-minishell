5a99cef0df8e49f5e9c5266713a67c2ad6e5db50
Killua <106412328+Killua0615@users.noreply.github.com>
2025-04-27 16:52:20 +0900

    [RFC] norminette

--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -1,28 +1,29 @@
 // builtin_cd.c
 #include "minishell.h"
 
-static void do_chdir_with_error(t_data *data, const char *path, const char *original_arg)
+static void	do_chdir_with_error(t_data *data, const char *path, const char *arg)
 {
 	if (chdir(path) < 0)
 	{
 		ft_dprintf(STDERR_FILENO, HEADER "cd: ");
-		perror(original_arg);
+		perror(arg);
 		data->exit_status = 1;
-		return;
+		return ;
 	}
 	data->exit_status = 0;
 }
 
-static void update_pwd_if_needed(t_data *data)
+static void	update_pwd_if_needed(t_data *data)
 {
+	char	cwd[PATH_MAX];
+
 	if (get_env(data->env, "PWD") != NULL)
 	{
-		char	cwd[PATH_MAX];
 		if (getcwd(cwd, sizeof(cwd)) == NULL)
 		{
 			ft_dprintf(STDERR_FILENO, "cd: getcwd: %s\n", strerror(errno));
 			data->exit_status = 1;
-			return;
+			return ;
 		}
 		set_env(&data->env, "PWD", cwd);
 	}
@@ -30,7 +31,7 @@ static void update_pwd_if_needed(t_data *data)
 
 static bool	handle_empty_or_abs(t_data *data, char **argv)
 {
-	char *home;
+	char	*home;
 
 	if (!argv[1] || argv[1][0] == '\0')
 	{
@@ -46,32 +47,34 @@ static bool	handle_empty_or_abs(t_data *data, char **argv)
 			if (data->exit_status == 0)
 				update_pwd_if_needed(data);
 		}
-		return true;
+		return (true);
 	}
-	return false;
+	return (false);
 }
 
-static void get_workdir_and_cd(t_data *data, const char *relpath)
+static void	get_workdir_and_cd(t_data *data, const char *relpath)
 {
-  char *pwd = get_env(data->env, "PWD");
-  if (!pwd)
-  {
-    static char buf[PATH_MAX];
-    if (getcwd(buf, sizeof(buf)) == NULL)
-    {
-      ft_dprintf(STDERR_FILENO, HEADER "getcwd: %s\n", strerror(errno));
-      data->exit_status = 1;
-      return;
-    }
-    pwd = buf;
-  }
-  char tmp[PATH_MAX];
-  ft_strlcpy(tmp, pwd, sizeof(tmp));
-  ft_strlcat(tmp, "/", sizeof(tmp));
-  ft_strlcat(tmp, relpath, sizeof(tmp));
-  do_chdir_with_error(data, tmp, relpath);
-  if (data->exit_status == 0)
-    update_pwd_if_needed(data);
+	char		*pwd;
+	static char	buf[PATH_MAX];
+	char		tmp[PATH_MAX];
+
+	pwd = get_env(data->env, "PWD");
+	if (!pwd)
+	{
+		if (getcwd(buf, sizeof(buf)) == NULL)
+		{
+			ft_dprintf(STDERR_FILENO, HEADER "getcwd: %s\n", strerror(errno));
+			data->exit_status = 1;
+			return ;
+		}
+		pwd = buf;
+	}
+	ft_strlcpy(tmp, pwd, sizeof(tmp));
+	ft_strlcat(tmp, "/", sizeof(tmp));
+	ft_strlcat(tmp, relpath, sizeof(tmp));
+	do_chdir_with_error(data, tmp, relpath);
+	if (data->exit_status == 0)
+		update_pwd_if_needed(data);
 }
 
 void	builtin_cd(t_data *data, char **argv)
@@ -79,16 +82,16 @@ void	builtin_cd(t_data *data, char **argv)
 	if (argv[1] == NULL)
 	{
 		handle_empty_or_abs(data, argv);
-		return;
+		return ;
 	}
 	if (argv[2] != NULL)
 	{
 		ft_dprintf(STDERR_FILENO, HEADER "cd: too many arguments\n");
 		data->exit_status = 1;
-		return;
+		return ;
 	}
 	if (handle_empty_or_abs(data, argv))
-		return;
+		return ;
 	if (argv[1][0] == '/')
 	{
 		do_chdir_with_error(data, argv[1], argv[1]);
