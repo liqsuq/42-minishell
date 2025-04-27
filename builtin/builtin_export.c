8e660bb7d030540445e0bd96c33f5c7cecc26d5d
liqsuq <liqsuq@rkgk.net>
2025-04-27 16:40:19 +0900

    [REF] builtin_export.cの短縮 無引数時の出力を削除

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -2,17 +2,7 @@
 
 #include "minishell.h"
 
-static void	print_env_export_format(t_env *env)
-{
-	while (env)
-	{
-		if (env->value)
-			printf("declare -x %s=\"%s\"\n", env->key, env->value);
-		env = env->next;
-	}
-}
-
-static int	is_valid_identifier(const char *name, size_t len)
+static int	is_valid_identifier(char *name, size_t len)
 {
 	size_t	i;
 
@@ -27,7 +17,7 @@ static int	is_valid_identifier(const char *name, size_t len)
 	return (1);
 }
 
-static int	check_name(t_data *data, const char *arg)
+static int	check_name(t_data *data, char *arg)
 {
 	char	*eq;
 	size_t	key_len;
@@ -46,38 +36,24 @@ static int	check_name(t_data *data, const char *arg)
 	return (1);
 }
 
-static void	process_arg(t_data *data, const char *arg)
+static void	export_arg(t_data *data, char *arg)
 {
 	char	*eq;
 	size_t	key_len;
-	char	*key;
 
 	eq = ft_strchr(arg, '=');
 	if (eq == NULL)
 		return ;
 	key_len = (size_t)(eq - arg);
-	key = ft_substr(arg, 0, key_len);
-	if (key == NULL)
-	{
-		data->exit_status = 1;
-		return ;
-	}
-	set_env(&data->env, key, eq + 1);
-	free(key);
+	arg[key_len] = '\0';
+	set_env(&data->env, arg, eq + 1);
+	arg[key_len] = '=';
 }
 
 void	builtin_export(t_data *data, char **argv)
 {
-	int	i;
-
 	data->exit_status = 0;
-	if (!argv[1])
-	{
-		print_env_export_format(data->env);
-		return ;
-	}
-	i = 0;
-	while (argv[++i])
-		if (check_name(data, argv[i]))
-			process_arg(data, argv[i]);
+	while (*++argv)
+		if (check_name(data, *argv))
+			export_arg(data, *argv);
 }
